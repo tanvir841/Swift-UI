af9bc036b9c977fcca3f1db7f2dcd0bfd0c16364
Abdullah Al Numan <ab>
2020-01-13 00:54:58 -0800

    Initial Commit

---
+++ Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI
+//
+//  Created by Tanvir on 1/13/20.
+//  Copyright © 2020 tanvir841. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
